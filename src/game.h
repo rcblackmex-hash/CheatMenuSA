a2fa9a58e27c9eb67d652c91100feb593274c444
rcblackmex-hash <rc.black.mex@gmail.com>
2026-05-15 22:29:12 -0700

    Update game.h

--- src/game.h
+++ src/game.h
@@ -9,9 +9,7 @@
 #include <string>
 #include <android/log.h>
 
-#define LOG_TAG "CheatMenuSA"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
 
 // ── Base del juego ───────────────────────────────────────────
 inline uintptr_t g_libGTASA = 0;
