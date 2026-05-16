fae16e0d54ea7001dfaabd735ae18156d73f25f9
rcblackmex-hash <rc.black.mex@gmail.com>
2026-05-15 22:42:21 -0700

    Update game.h

--- src/game.h
+++ src/game.h
@@ -5,9 +5,14 @@
 //  Librería: libGTASA.so — ELF 64-bit ARM aarch64, not stripped
 // ============================================================
 
+#pragma once
 #include <stdint.h>
 #include <string>
 #include <android/log.h>
+
+#define LOG_TAG "CheatMenuSA"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 
 
