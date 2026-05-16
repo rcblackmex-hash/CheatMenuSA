e28c4c322fdc9fcb1438396b84f6e2ebb0276803
rcblackmex-hash <rc.black.mex@gmail.com>
2026-05-15 18:39:19 -0700

    Update AML.h

--- sdk/aml/include/AML/AML.h
+++ sdk/aml/include/AML/AML.h
@@ -2,17 +2,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <dlfcn.h>
+
 enum class AMLModType { Lib=0, Tool=1, Patcher=2 };
-typedef void (*AMLMainFn)();
-struct AMLModInfo {
-    const char* szGUID;
-    const char* szName;
-    const char* szVersion;
-    const char* szAuthor;
-    const char* szDescription;
-    AMLMainFn   fnMain;
-    AMLModType  eType;
-};
+
 class IAML {
 public:
     virtual ~IAML(){}
@@ -28,5 +20,21 @@ public:
     virtual bool IsGameVersion(int v){ return true; }
     virtual uintptr_t PatternScan(const char* p, const char* m){ return 0; }
 };
+
+typedef void (*AMLMainFn)(IAML* aml);
+
+#define MOD_MAIN AML_OnModMain
+
+struct AMLModInfo {
+    const char* szGUID;
+    const char* szName;
+    const char* szVersion;
+    const char* szAuthor;
+    const char* szDescription;
+    AMLMainFn   fnMain;
+    AMLModType  eType;
+};
+
 #define AML_MOD_DEFINE static AMLModInfo __aml_modinfo =
-#define MOD_MAIN []()
+
+void AML_OnModMain(IAML* aml);
